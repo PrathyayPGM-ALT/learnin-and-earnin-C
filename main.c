9670a095242cb3830d534c142a1a0de0179da6f2
Fatal <prathyaym@gmail.com>
2025-12-06 11:50:45 +0530

    Create main.c

---
+++ main.c
@@ -0,0 +1,16 @@
+#include <stdio.h> //wth even is studio.h
+#include <stdbool.h> //bro needs an entire library for booleans
+
+int main(void) {
+  printf("Semicolon go BRRRRR\n");
+
+  for(int i = 0; i < 5; i++) {
+    printf("Who puts curly braces in a for loop?\n");
+  }
+
+  while (true) { //t isnt CAPITALIZED WTH
+    printf("lol\n");
+  }
+
+  return 0; //for what joy
+}
